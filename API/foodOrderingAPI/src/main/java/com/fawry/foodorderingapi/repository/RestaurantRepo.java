1c1f6e34f3f63a2af4f3240698871ab5a3bc8c5a
Tawfik  Shalash <74143032+tawfik-s@users.noreply.github.com>
2022-12-23 15:40:10 +0200

    add repositories

---
+++ API/foodOrderingAPI/src/main/java/com/fawry/foodorderingapi/repository/RestaurantRepo.java
@@ -0,0 +1,9 @@
+package com.fawry.foodorderingapi.repository;
+
+import com.fawry.foodorderingapi.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepo extends JpaRepository<Restaurant,Long> {
+}
