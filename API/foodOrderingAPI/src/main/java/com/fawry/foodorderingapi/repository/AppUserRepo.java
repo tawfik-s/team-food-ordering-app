60deabf99ca3fb53250c30d6451580bf5d14785e
Tawfik  Shalash <74143032+tawfik-s@users.noreply.github.com>
2023-01-06 18:52:25 +0200

    reduce complexity in finish group and use jpql to do that

--- API/foodOrderingAPI/src/main/java/com/fawry/foodorderingapi/repository/AppUserRepo.java
+++ API/foodOrderingAPI/src/main/java/com/fawry/foodorderingapi/repository/AppUserRepo.java
@@ -1,13 +1,22 @@
 package com.fawry.foodorderingapi.repository;
 
+import com.fawry.foodorderingapi.entity.AppGroup;
 import com.fawry.foodorderingapi.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AppUserRepo extends JpaRepository<AppUser,Long> {
 
     Optional<AppUser> findByEmail(String email);
+
+    @Query("select g from AppUser u " +
+            "join u.OwnedGroups g where u.id=:userId" +
+            " and g.id =:groupId")
+    List<AppGroup> returnOwnedGroup(@Param("userId") Long userId, @Param("groupId") Long groupId);
 }
